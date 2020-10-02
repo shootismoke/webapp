5e888c15426aa8653540b45460726762019d4042
Amaury Martiny <amaury.martiny@protonmail.com>
2020-10-02 22:30:27 +0200

    Add city cards

---
+++ src/components/RankingSection/CityCard/CityCard.tsx
@@ -0,0 +1,49 @@
+// Shoot! I Smoke
+// Copyright (C) 2018-2020  Marcelo S. Coelho, Amaury Martiny
+
+// Shoot! I Smoke is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Shoot! I Smoke is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Shoot! I Smoke.  If not, see <http://www.gnu.org/licenses/>.
+
+import React from 'react';
+import c from 'classnames';
+
+import { Card } from '../../Card';
+
+const TMP_IMAGE =
+	'https://images.pexels.com/photos/1563256/pexels-photo-1563256.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+
+interface CityCardProps {
+	description?: string;
+	className?: string;
+	subtitle: string;
+	title: string;
+}
+
+export function CityCard(props: CityCardProps): React.ReactElement {
+	const { className, description, subtitle, title } = props;
+
+	return (
+		<Card
+			className={c('my-4 shadow-lg h-24 pr-2 flex flex-row', className)}
+		>
+			<img className="mr-4 w-24 h-24" src={TMP_IMAGE} />
+			<div className="flex flex-col justify-center">
+				<h4 className="mb-1 text-xl font-gotham-black text-orange">
+					{title}
+				</h4>
+				<h5 className="text-sm">{subtitle}</h5>
+				<p className="text-xs text-gray-600">{description}</p>
+			</div>
+		</Card>
+	);
+}
--- src/components/RankingSection/RankingSection.tsx
+++ src/components/RankingSection/RankingSection.tsx
@@ -21,11 +21,29 @@ import React from 'react';
 import { City } from '../../util';
 import { Section } from '../Section';
 import { SectionDivider } from '../SectionDivider';
+import { CityCard } from './CityCard';
+
+/**
+ * Convert a number to an ordinal, e.g. 1 to 1st.
+ * @param n - The number to convert to ordinal.
+ */
+function numberToOrdinal(n: number): string {
+	switch (n) {
+		case 1:
+			return '1st';
+		case 2:
+			return '2nd';
+		case 3:
+			return '3rd';
+		default:
+			return `${n}th`;
+	}
+}
 
 export function RankingSection(): React.ReactElement {
 	const cities = useStaticQuery(graphql`
 		query AllCitiesQuery {
-			allShootismokeCity(limit: 10) {
+			allShootismokeCity(limit: 5) {
 				nodes {
 					api {
 						shootismoke {
@@ -41,23 +59,28 @@ export function RankingSection(): React.ReactElement {
 
 	return (
 		<>
-			<SectionDivider title="City ranking" />
-			<Section>
-				<ul>
-					{cities.allShootismokeCity.nodes.map((city: City) => (
-						<li key={city.slug}>
+			<SectionDivider title="Worldwide City ranking" />
+			<Section className="flex flex-col items-center">
+				<div className="max-w-6xl">
+					{cities.allShootismokeCity.nodes.map(
+						(city: City, index: number) => (
 							<Link
-								className="underline"
+								className="w-full "
+								key={city.slug}
 								to={`/city/${city.slug}`}
 							>
-								{city.name}
+								<CityCard
+									description={`${round(
+										city.api?.shootismoke.dailyCigarettes ||
+											0
+									)} cigarettes today`}
+									subtitle={city.name || ''}
+									title={`${numberToOrdinal(index + 1)}`}
+								/>
 							</Link>
-							:{' '}
-							{round(city.api?.shootismoke.dailyCigarettes || 0)}{' '}
-							cigarettes
-						</li>
-					))}
-				</ul>
+						)
+					)}
+				</div>
 			</Section>
 		</>
 	);
