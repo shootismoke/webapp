211e82f7f687a6e4fabe2c914f5814255ec350cf
Amaury Martiny <amaury.martiny@protonmail.com>
2020-10-26 10:14:53 +0100

    Fix lint

--- src/util/amplitude.ts
+++ src/util/amplitude.ts
@@ -14,6 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Sh**t! I Smoke.  If not, see <http://www.gnu.org/licenses/>.
 
+// Import the `amplitude` namespace for TS typings.
+import 'amplitude-js';
+
 import { sentryException } from './sentry';
 
 /**
