1881a879731d2d83c502eb6b78fa09232af74778
Amaury Martiny <amaury.martiny@protonmail.com>
2020-10-31 20:43:11 +0100

    Remove should

--- cypress/integration/components/SearchBar.ts
+++ cypress/integration/components/SearchBar.ts
@@ -30,14 +30,12 @@ function performSearch(
 
 			cy.get('[data-cy=SearchBar-AsyncSelect] input')
 				.click({ force: true })
-				.should('be.focused', { force: true })
 				.type(searchInput, { force: true });
 
 			// See https://stackoverflow.com/questions/55046835/select-react-select-dropdown-list-option-using-cypress
 			cy.get('[data-cy=SearchBar-AsyncSelect]')
 				.get('[class*="-menu"]')
 				.find('[class*="-option"]')
-				.should('have.length.gt', 1)
 				.first()
 				.click({ force: true });
 
