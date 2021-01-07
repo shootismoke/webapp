139fe47cd6ce362bd41c26346639292884c53b2f
Marcelo S. Coelho <35773925+marceloscoelho@users.noreply.github.com>
2021-01-07 22:05:52 +0100

    feat: colors
    Changing the greys and the orange for better accessibility / contrast.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@ module.exports = {
 	purge: ['./src/**/*.tsx'],
 	theme: {
 		colors: {
-			'gray-200': '#CCCFD5',
-			'gray-600': '#777D89',
-			'gray-700': '#44464A',
+			'gray-200': '#CCCFD4',
+			'gray-600': '#646973',
+			'gray-700': '#3D3F42',
 			white: '#FFFFFF',
 			cream: '#F7F0DF',
 			gold: 'E5D98D',
-			orange: '#F8A65D',
+			orange: '#F2934E',
 			red: '#FF5139',
 		},
 	},
