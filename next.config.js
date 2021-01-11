f3172b4c02094444345cf3e446abeac9885b433f
Amaury Martiny <amaury.martiny@protonmail.com>
2021-01-11 18:02:16 +0100

    Remove expo

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-// @see https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#withexpo
-const { withExpo } = require('@expo/next-adapter');
 const withFonts = require('next-fonts');
 const withImages = require('next-images');
 
-module.exports = withExpo(
-	withFonts(
-		withImages({
-			projectRoot: __dirname,
-		})
-	)
+module.exports = withFonts(
+	withImages({
+		customExpoConfig,
+	})
 );
