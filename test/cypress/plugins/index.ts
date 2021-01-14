dfa91631b922447a3439580dd3707da096263074
Amaury Martiny <amaury.martiny@protonmail.com>
2021-01-14 21:32:30 +0100

    Use ts for cypress

--- test/cypress/plugins/index.ts
+++ test/cypress/plugins/index.ts
@@ -12,10 +12,13 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+/// <reference types="cypress" />
+
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+module.exports = () => {
+	return;
+};
+
+export {};
