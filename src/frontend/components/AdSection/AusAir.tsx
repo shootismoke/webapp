aed7eb99341935fdffde84ab0c3e9aafd077b507
Amaury M <1293565+amaurym@users.noreply.github.com>
2021-03-09 11:28:29 +0100

    Better responsiveness

--- src/frontend/components/AdSection/AusAir.tsx
+++ src/frontend/components/AdSection/AusAir.tsx
@@ -18,7 +18,7 @@
 import Image from 'next/image';
 import React from 'react';
 
-import dreadsWide from '../../../../assets/images/ausair/dreads-guy-cropped-wide.png';
+import dreadsWide from '../../../../assets/images/ausair/dreads-guy-cropped-wide.webp';
 import product8 from '../../../../assets/images/ausair/product-8.png';
 import { logEvent } from '../../util';
 import { Button } from '../Button';
@@ -26,11 +26,15 @@ import { Button } from '../Button';
 export function AusAir(): React.ReactElement {
 	return (
 		<div
+			// Grid is:
+			// - no-grid (top/bottom) on small screens
+			// - 4/7 | 3/7 on medium screens
+			// - 2/3 | 1/3 on large screens
 			className="border border-gray-200 rounded-xl shadow-md overflow-hidden
 				h-64
-				md:grid md:grid-cols-3"
+				md:grid md:grid-cols-7 lg:grid-cols-3"
 		>
-			<div className="overflow-hidden h-32 md:h-64 md:col-span-2">
+			<div className="overflow-hidden h-32 md:h-64 md:col-span-4 lg:col-span-2">
 				<div className="next-images relative w-full h-full">
 					<Image
 						alt="cigarettes-conversion"
@@ -41,7 +45,7 @@ export function AusAir(): React.ReactElement {
 					/>
 				</div>
 			</div>
-			<div className="px-6 py-4 flex flex-col justify-between items-center md:items-start">
+			<div className="md:col-span-3 lg:col-span-1 px-6 py-4 flex flex-col justify-between items-center md:items-start">
 				<div className="hidden md:block next-images relative w-32 h-24">
 					<Image
 						alt="cigarettes-conversion"
@@ -69,12 +73,9 @@ export function AusAir(): React.ReactElement {
 					rel="noreferrer"
 					target="_blank"
 				>
-					<Button className="mt-2 px-10 md:px-6 py-2 text-center">
+					<Button className="mt-2 px-10 md:px-4 lg:px-8 py-2 text-center">
 						<div className="leading-4 type-300 text-orange uppercase">
-							<span className="inline md:hidden lg:inline">
-								Save{' '}
-							</span>
-							10% with code
+							Save 10% with code
 						</div>
 						<div className="leading-4 type-300 text-orange uppercase">
 							&apos;STOPSMOKING&apos;
