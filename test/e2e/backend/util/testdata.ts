e9d9d692ddf2d46c9e9bcfbc3b975814d7245b95
Amaury M <1293565+amaurym@users.noreply.github.com>
2022-10-07 22:10:45 +0200

    fix: Fix ci

--- test/e2e/backend/util/testdata.ts
+++ test/e2e/backend/util/testdata.ts
@@ -15,6 +15,10 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+import { config } from 'dotenv';
+
+config({ path: '.env' });
+
 import { secretHeader } from '../../../../src/backend/util';
 
 export const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:3000';
